8f06abe20f824a8f6da25b9ad6dea51dfed4bb90
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2015-07-20 13:19:58 -0400

    First commit

--- BinaryTimer/ViewController.swift
+++ BinaryTimer/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelForBinaryCount: UILabel!
+    @IBOutlet weak var labelForRegularCount: UILabel!
+    
+    var timer = NSTimer()
+    var binaryCount = 0b00000
+    var normalCount = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func start(sender: UIButton) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "countUp", userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func reset(sender: UIButton) {
+        timer.invalidate()
+        binaryCount = 0b0000
+        updateText()
+    }
+    
+    func countUp() {
+        binaryCount += 0b00001
+        normalCount += 1
+        if (binaryCount == 0b100000) { binaryCount = 0b00000 }
+        if (normalCount == 32) { normalCount = 0 }
+        updateText()
+    }
+    
+    func updateText() {
+        var text = String(binaryCount, radix:2)
+        for i in 0..<5 - count(text) {
+            text = "0" + text;
+        }
+        labelForBinaryCount.text = text
+        labelForRegularCount.text = String(normalCount)
+    }
 
 }
-
